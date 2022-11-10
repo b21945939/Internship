6db93ba12cd184d1d5b4b2f00f72005327d4fd16
b21945939 <72977132+b21945939@users.noreply.github.com>
2022-11-10 17:07:10 +0300

    Add files via upload

---
+++ GameFinder/GameFinder.Entities/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameFinder.Entities
+{
+    public class Game
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id {get;set;}
+        public string Name { get; set; }
+        public string Types { get; set; }
+        public string Platforms { get; set; }
+        public int MetaScore { get; set; }
+        public double UserScore { get; set; }
+        public string Image { get; set; }
+        public string Video { get; set; }
+    }
+}
